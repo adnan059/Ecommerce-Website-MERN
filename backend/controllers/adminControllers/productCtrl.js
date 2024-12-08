4ad24f9e6888824d7e99c2075b6edbb3f00fa277
adnan059 <adnanfaruk83@gmail.com>
2024-12-09 02:07:22 +0600

    up

--- backend/controllers/adminControllers/productCtrl.js
+++ backend/controllers/adminControllers/productCtrl.js
@@ -1,3 +1,4 @@
+const Product = require("../../models/Product");
 const { imageUploadUtil } = require("../../utils/helpers");
 
 // upload product image
@@ -14,11 +15,79 @@ const imageUploadCtrl = async (req, res, next) => {
 };
 
 // add a product
-const addProduct = async (req, res, next) => {};
+const addProduct = async (req, res, next) => {
+  try {
+    const {
+      image,
+      title,
+      description,
+      category,
+      brand,
+      price,
+      salePrice,
+      totalStock,
+      averageReview,
+    } = req.body;
+
+    const newProduct = await Product.create({
+      image,
+      title,
+      description,
+      category,
+      brand,
+      price,
+      salePrice,
+      totalStock,
+      averageReview,
+    });
+
+    res.status(201).json(newProduct);
+  } catch (error) {
+    next(error);
+  }
+};
 
 // fetch all products
+const fetchAllProducts = async (req, res, next) => {
+  try {
+    const productList = await Product.find({});
+    res.status(200).json(productList);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// edit a product
+const editProduct = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// delete a product
+const deleteProduct = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    await Product.findByIdAndDelete(id);
+    res.status(200).json({
+      message: "product deleted successfully",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports = {
   imageUploadCtrl,
   addProduct,
+  fetchAllProducts,
+  editProduct,
+  deleteProduct,
 };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,11 +2,16 @@ const express = require("express");
 const { upload } = require("../utils/helpers");
 const {
   imageUploadCtrl,
+  addProduct,
+  editProduct,
+  deleteProduct,
+  fetchAllProducts,
 } = require("../controllers/adminControllers/productCtrl");
 const { verifyToken, verifyAdmin } = require("../utils/verify");
 
 const router = express.Router();
 
+// image upload
 router.post(
   "/upload-image",
   verifyToken,
@@ -14,5 +19,13 @@ router.post(
   upload.single("my-file"),
   imageUploadCtrl
 );
+
+router.post("/add", verifyToken, verifyAdmin, addProduct);
+
+router.put("/edit/:id", verifyToken, verifyAdmin, editProduct);
+
+router.delete("/delete/:id", verifyToken, verifyAdmin, deleteProduct);
+
+router.get("/all", fetchAllProducts);
 
 module.exports = router;
